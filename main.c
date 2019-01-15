d5a5ea184890b7658c1d2b62bd677aba824e72a8
Your Name <you@example.com>
2019-01-15 15:49:53 +0800

    修改余额可能为负的bag

--- main.c
+++ main.c
@@ -1,50 +1,62 @@
 #include <stdio.h>
 
-int main(){
-    int qian=0;
-    printf("jingruqianbaoguanlixitong\n");    
-    while(1){
+int main()
+{
+    int qian = 0;
+    printf("jingruqianbaoguanlixitong\n");
+    while (1)
+    {
         printf("1--cunqian\n");
         printf("2--quqian\n");
         printf("3--yue\n");
         printf("4--tuichu\n");
 
-        int code=0;
+        int code = 0;
         printf("qingshuruxinagxingdegongnengbianhao:\n");
-        scanf("%d",&code);
-        if(code==1){
+        scanf("%d", &code);
+        if (code == 1)
+        {
             printf("qingshurunidecunkuanjine:\n");
             int cun;
-            scanf("%d",&cun);
+            scanf("%d", &cun);
             char x;
-            qian+=cun;
+            qian += cun;
             printf("cunruchenggong,dianjihuichejixu\n");
-            scanf("%c",&x);
-            scanf("%c",&x);
-
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
-        if(code==2){
-            printf("qingchuruquqiandegongneng:\n");
+        if (code == 2)
+        {
+            printf("qingchuruqukuanjinge:\n");
             int qu;
-            scanf("%d",&qu);
-            
-            qian-=qu;
+            scanf("%d", &qu);
+            if (qu > qian)
+            {
+               printf("xuebuzu,qukuanshibai,dianjihuichejixu\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
+            else
+            {
+                qian-=qu;
+                printf("quchuchenggong,dianjihuichejixu\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
 
-            printf("qukuanchenggong,dianjihuichejixu\n");
-            char x;
-            scanf("%c",&x);
-            scanf("%c",&x);
-
-            
+            qian -= qu;
         }
-        if(code==3){
-            
+        if (code == 3)
+        {
+            printf("dangqianyueewei:\n");
+            printf("%d\n", qian);
         }
-        if(code==4){
-            
+        if (code == 4)
+        {
         }
     }
 
     return 0;
-
 }
